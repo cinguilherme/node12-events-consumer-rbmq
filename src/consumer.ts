a3b96f8267033a298de6486a783d46f7e3b8b695
Guilherme Cintra (gcintra2) <cinguilherme@gmail.com>
2020-06-22 20:12:18 -0300

    fix eslint alerts

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,4 +1,4 @@
-import Message from './models/message'
+
 import { openChannel } from './shared/rabbitConnection'
 
 const consumeMessages = async () => {
@@ -7,7 +7,7 @@ const consumeMessages = async () => {
     if (message?.content) {
       channel.ack(message)
       const content = message.content.toString('utf-8')
-      const msg:Message = JSON.parse(content)
+      const msg = JSON.parse(content)
       console.log('consuming message', msg)
     }
   })
