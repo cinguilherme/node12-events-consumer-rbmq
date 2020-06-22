820a3d7895ff9a8e35f588aeb604ef269fc268ea
Guilherme Cintra (gcintra2) <cinguilherme@gmail.com>
2020-06-22 19:42:53 -0300

    test producer running

---
+++ src/models/message.ts
@@ -0,0 +1,11 @@
+class Message {
+    title:string
+    text:string
+
+    constructor (title, message) {
+      this.text = message
+      this.title = title
+    }
+}
+
+export default Message
--- src/producer.ts
+++ src/producer.ts
@@ -1,3 +1,46 @@
-import 'amqplib'
 
-console.log('this is a test producer..')
+import amqp from 'amqplib'
+import Message from './models/message'
+
+const getOpenConn = async () => {
+  var connectionObject = {
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5672,
+    username: 'admin',
+    password: 'admin',
+    frameMax: 0,
+    heartbeat: 1,
+    vhost: '/'
+  }
+
+  const conn = await amqp.connect(connectionObject)
+
+  return conn
+}
+
+const openChannel = async () => {
+  var openConn = await getOpenConn()
+  var channel = await openConn.createChannel()
+
+  return channel
+}
+
+const produceMessages = async () => {
+  const channel = await openChannel()
+
+  process.on('exit', (code) => {
+    channel.close()
+    console.log('Closing rabbitmq channel')
+  })
+
+  const data = JSON.stringify(new Message('title', 'text'))
+
+  setInterval(() => {
+    channel.sendToQueue('messages', new Buffer(data))
+  }, 100)
+}
+
+produceMessages()
+
+console.log('test producer running..')
